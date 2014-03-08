7b8f3b2b586e8a0d5bc9989e71a32cd440c21aba
Zhen Jiang <jiang.zhenhang@gmail.com>
2014-03-08 12:59:41 -0500

    Create procedure for ui data

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,17 @@
 -- This file must import into database named capstone
 
+-- use the following command to import this sql file into capstone db
+-- mysql -u zhen --pass="Zhenjiang#1" capstone < seed.sql
 
 -- create tables
+
+DROP TABLE IF EXISTS xyz_tmp;
+CREATE TABLE xyz_tmp (
+    id INT NOT NULL AUTO_INCREMENT,
+    value VARCHAR(100) NOT NULL,
+    output VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 DROP TABLE IF EXISTS xyz;
 CREATE TABLE xyz (
@@ -24,13 +34,6 @@ CREATE TABLE ui_xyz (
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS ui_command;
-CREATE TABLE ui_command (
-    id INT NOT NULL AUTO_INCREMENT,
-    input VARCHAR(100) NOT NULL,
-    PRIMARY KEY(id)
-);
-
 -- craete procedures
 
 DROP PROCEDURE IF EXISTS get_xyz;
@@ -39,7 +42,8 @@ CREATE PROCEDURE get_xyz(OUT rs CHAR(100))
 BEGIN
     DECLARE val_id INT;
     IF(EXISTS(SELECT value FROM xyz LIMIT 1)) THEN
-        SELECT value INTO rs FROM xyz ORDER BY id DESC LIMIT 1;
+        SELECT value, id INTO rs, val_id FROM xyz ORDER BY id DESC LIMIT 1;
+        UPDATE xyz_tmp set output="1" where id = val_id;
         DELETE FROM xyz;
     END IF;
 END/
@@ -63,20 +67,8 @@ CREATE PROCEDURE get_new_data(OUT rs CHAR(100))
 BEGIN
     DECLARE val_id INT;
     IF(EXISTS(SELECT input FROM command LIMIT 1)) THEN
-        SELECT input INTO rs FROM command ORDER BY id ASC LIMIT 1;
-        DELETE FROM command;
-    END IF;
-END/
-delimiter ;
-
-DROP PROCEDURE IF EXISTS ui_get_new_data;
-delimiter /
-CREATE PROCEDURE ui_get_new_data(OUT rs CHAR(100))
-BEGIN
-    DECLARE val_id INT;
-    IF(EXISTS(SELECT input FROM ui_command LIMIT 1)) THEN
-        SELECT input INTO rs FROM ui_command ORDER BY id ASC LIMIT 1;
-        DELETE FROM ui_command;
+        SELECT input, id INTO rs, val_id FROM command ORDER BY id ASC LIMIT 1;
+        DELETE FROM command WHERE id = val_id LIMIT 1;
     END IF;
 END/
 delimiter ;
@@ -89,7 +81,22 @@ BEGIN
 END/
 delimiter ;
 
+DROP PROCEDURE IF EXISTS add_ui_input;
+delimiter /
+CREATE PROCEDURE add_ui_input(IN input CHAR(100))
+BEGIN
+     INSERT INTO ui_xyz VALUES(DEFAULT, input);
+END/
+delimiter ;
 
-
-
-
+DROP PROCEDURE IF EXISTS get_ui_data;
+delimiter /
+CREATE PROCEDURE get_ui_data(OUT rs CHAR(100))
+BEGIN
+    DECLARE val_id INT;
+    IF(EXISTS(SELECT value FROM ui_xyz LIMIT 1)) THEN
+        SELECT value, id INTO rs, val_id FROM ui_xyz ORDER BY id ASC LIMIT 1;
+        DELETE FROM ui_xyz WHERE id = val_id LIMIT 1;
+    END IF;
+END/
+delimiter ;
